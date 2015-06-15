b273d668d170c4106e61f6e4ed6fb792a65dc176
jacksky007 <jacksky007@qq.com>
2015-06-15 23:05:53 +0800

    remove debugging code

--- flatten-thunk/index.js
+++ flatten-thunk/index.js
@@ -16,21 +16,3 @@ function flattenThunk(thunk) {
   }
 }
 
-var thunk1 = function(cb) {
-  setTimeout(function() {
-    cb(null, 'done');
-  }, 1);
-}
-var thunk2 = function(cb) {
-  setTimeout(function() {
-    cb(null, thunk1);
-  }, 1);
-} 
-var thunk3 = function(cb) {
-  setTimeout(function() {
-    cb(null, thunk2);
-  }, 1);
-}
-flattenThunk(thunk3)(function(err, result) {
-  console.log(3, err, result)
-})
