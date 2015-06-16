29d7a58b3c824205e1dd3d53830d93bc3afd7197
jacksky007 <jacksky007@qq.com>
2015-06-17 00:11:20 +0800

    debounce by setImmediate

--- debounce/index.js
+++ debounce/index.js
@@ -1,6 +1,23 @@
 'use strict'
 
 module.exports = function(f, delay) {
+  var immediate
+  return function() {
+    var me = this
+    var args = [].slice.call(arguments)
+    clearImmediate(immediate)
+    var d0 = new Date
+    ;(function fn() {
+      immediate = setImmediate(function() {
+        var d1 = new Date
+        if (d1 - d0 > delay) {
+          return f.apply(me, args)
+        }
+        fn()
+      })
+    })()
+  }
+
   var timeout
   return function() {
     var me = this
