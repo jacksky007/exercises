79cfd44e674e24845f5e1e0344f9951b0d2a9d2b
xiaojie.liu <xiaojie.liu@ifchange.com>
2015-06-18 17:49:39 +0800

    currying: bind context

--- curry/index.js
+++ curry/index.js
@@ -1,16 +1,23 @@
 'use strict'
 
-module.exports = function(f) {
+module.exports = curry
+function curry(f) {
   function _curry(f, argsCount, params) {
+    var context = this
     if (params.length >= argsCount) {
-      return f.apply(null, params)
+      return f.apply(context, params)
     }
     return function() {
-      return _curry(f.bind.apply(f, [null].concat(params)), argsCount - params.length, [].slice.call(arguments))
+      return _curry.call(
+          context,
+          f.bind.apply(f, [context].concat(params)),
+          argsCount - params.length,
+          [].slice.call(arguments)
+      )
     }
   }
   return function() {
-    return _curry(f, f.length, [].slice.call(arguments))
+    return _curry.call(this, f, f.length, [].slice.call(arguments))
   }
 }
 
