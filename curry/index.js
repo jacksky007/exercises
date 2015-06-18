2fd5a8764e7f2ee1fa4f401b1463efd797e00c33
jacksky007 <jacksky007@qq.com>
2015-06-18 22:29:53 +0800

    currying: fix context binding

--- curry/index.js
+++ curry/index.js
@@ -1,22 +1,20 @@
 'use strict'
 
 module.exports = function(f) {
-  function _curry(f, argsCount, params) {
-    var context = this
+  var argsCount = f.length
+  function args2Array(args) {
+    return [].slice.call(args)
+  }
+  function _curry(context, params) {
     if (params.length >= argsCount) {
       return f.apply(context, params)
     }
     return function() {
-      return _curry.call(
-          context,
-          f.bind.apply(f, [this || context].concat(params)),
-          argsCount - params.length,
-          [].slice.call(arguments)
-      )
+      return _curry(this || context, params.concat(args2Array(arguments)))
     }
   }
   return function() {
-    return _curry.call(this, f, f.length, [].slice.call(arguments))
+    return _curry(this, args2Array(arguments))
   }
 }
 
