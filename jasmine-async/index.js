2c07de62bc89117ac301755a2c85b4bb292e229c
jacksky007 <jacksky007@qq.com>
2015-06-27 21:29:57 +0800

    jasmin-async passed

--- jasmine-async/index.js
+++ jasmine-async/index.js
@@ -1,7 +1,19 @@
 'use strict'
 
 module.exports = function(f) {
+  var done
+  function isDone() {
+    done = true
+  }
   var test = f()
-  test.setup(test.test)
+  it(test.desc, function() {
+    runs(function() {
+      test.setup(isDone)
+    })
+    waitsFor(function() {
+      return done
+    })
+    runs(test.test)
+  })
 }
 
